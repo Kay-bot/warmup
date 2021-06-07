4a2aed063e5dfe45ed62a03c0cee557b371cf5c5
KAY S <KS.KWANRUAN@GMAIL.com>
2021-06-08 00:47:54 +1000

    refactor

--- JSON/index.js
+++ JSON/index.js
@@ -1,59 +1,56 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
+
 let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
 
-request.onload = function() {
-    const superHeroes = request.response;
-    populateHeader(superHeroes);
+fetch(requestURL).then(response => response.json()).then(json => {
+    const superHeroes = json
+    populateHeader(superHeroes)
     showHeroes(superHeroes)
-}
+})
 
-function populateHeader(obj) {
+const populateHeader = (obj) => {
     const myH1 = document.createElement('h1');
     myH1.textContent = obj.squadName
     header.appendChild(myH1)
 
     const myPara = document.createElement('p');
     myPara.textContent = `
-    Hometown: ${obj.homeTown}
-    Formed: ${obj.formed}
-   `
+Hometown: ${obj.homeTown}
+Formed: ${obj.formed}
+`
     header.appendChild(myPara)
-}
+};
 
-function showHeroes(obj) {
+const showHeroes = (obj) => {
     const heroes = obj.members;
 
-    for (let i = 0; i < heroes.length; i++) {
+    heroes.map(h => {
+
+        const Superpowers = h.powers
+
         const myArticle = document.createElement('article');
         const myH2 = document.createElement('h2');
         const myPara1 = document.createElement('p');
         const myPara2 = document.createElement('p');
         const myPara3 = document.createElement('p');
         const myList = document.createElement('ul');
+        const listItem = document.createElement('li');
 
-        myH2.textContent = heroes[i].name;
-        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-        myPara2.textContent = 'Age: ' + heroes[i].age;
+        myH2.textContent = h.name;
+        myPara1.textContent = 'Secret identity: ' + h.secretIdentity;
+        myPara2.textContent = 'Age: ' + h.age;
         myPara3.textContent = 'Superpowers:';
-
-        const superPowers = heroes[i].powers;
-        for (let j = 0; j < superPowers.length; j++) {
-            const listItem = document.createElement('li');
-            listItem.textContent = superPowers[j];
-            myList.appendChild(listItem);
-        }
+        listItem.textContent = `${Superpowers.join(", ")}`
 
         myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
         myArticle.appendChild(myList);
-
+        myList.appendChild(listItem);
         section.appendChild(myArticle);
-    }
-}
+
+    })
+
+};
